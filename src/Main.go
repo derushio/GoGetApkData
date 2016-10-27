1a5ae32b5d6f49c5dd9b2305e487c45307e57a97
derushio <narumi18wa@gmail.com>
2016-10-27 23:55:48 +0900

    クリーン機能実装

--- src/Main.go
+++ src/Main.go
@@ -7,12 +7,25 @@ import (
 
 func main() {
 	var packageName string
+	var clean bool
 
+	// args1 パッケージ名
 	if len(os.Args) >= 2 {
 		packageName = os.Args[1]
 	} else {
 		fmt.Println("取得するアプリデータのパッケージ名を入力してください")
 		fmt.Scanln(&packageName)
+	}
+
+	// args2 クリーンの可否
+	clean = false
+	if len(os.Args) >= 3 {
+		if os.Args[2] == "clean" {
+			clean = true
+		}
+	}
+	if clean == true {
+		os.RemoveAll(packageName)
 	}
 
 	fmt.Println("パッケージ `" + packageName + "` からデータを取得します")
